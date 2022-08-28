495692541979bc3d45804785c5ee904665897fe0
gustavocern <gustavoargueta363@gmail.com>
2022-08-27 21:07:27 -0500

    Formulario de Autores.

---
+++ Autores.Designer.cs
@@ -0,0 +1,98 @@
+﻿
+namespace Semana_5_proyecto_formularios_biblioteca
+{
+    partial class Autores
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listViewCategoria = new System.Windows.Forms.ListView();
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // listViewCategoria
+            // 
+            this.listViewCategoria.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.listViewCategoria.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader3});
+            this.listViewCategoria.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(254)));
+            this.listViewCategoria.GridLines = true;
+            this.listViewCategoria.HideSelection = false;
+            this.listViewCategoria.Location = new System.Drawing.Point(59, 191);
+            this.listViewCategoria.Margin = new System.Windows.Forms.Padding(2);
+            this.listViewCategoria.Name = "listViewCategoria";
+            this.listViewCategoria.Size = new System.Drawing.Size(519, 180);
+            this.listViewCategoria.TabIndex = 24;
+            this.listViewCategoria.UseCompatibleStateImageBehavior = false;
+            this.listViewCategoria.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "Autor.";
+            this.columnHeader3.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.columnHeader3.Width = 247;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Año.";
+            this.columnHeader1.Width = 153;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(239, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(170, 20);
+            this.label1.TabIndex = 25;
+            this.label1.Text = "Formulario de Autores.";
+            // 
+            // Autores
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightGreen;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listViewCategoria);
+            this.Name = "Autores";
+            this.Text = "Autores";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView listViewCategoria;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.Label label1;
+    }
+}
